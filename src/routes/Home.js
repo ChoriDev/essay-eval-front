9e8518e887153ca2fef4b5ef8603fd30844527c2
Chori <yidodeveloper@gmail.com>
2024-03-19 21:45:01 +0900

    design: 네비게이션바 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import styles from "../css/Home.module.css";
 import submitIcon from "../images/submitIcon.png";
+import Navbars from "../components/Navbars";
 
 function Home() {
   const [letterCount, setLetterCount] = useState(0);
@@ -11,33 +12,34 @@ function Home() {
   };
 
   return (
-    <div className={styles.container}>
-      {/* TODO header 태그를 Navigation component로 변경 */}
-      <header className="navigation" />
-      <Form>
-        <Form.Group className={styles.textBox}>
-          <Form.Label>하단에 에세이를 입력해주세요.</Form.Label>
-          <Form.Control
-            className={styles.textArea}
-            as={"textarea"}
-            rows={13}
-            maxLength={1000}
-            onChange={changeLetterCount}
-          />
-        </Form.Group>
-        <div className={styles.letterCountBox}>
-          <div className={styles.letterCount}>
-            현재 글자수: {letterCount} / 1000
+    <>
+      <Navbars />
+      <div className={styles.container}>
+        <Form>
+          <Form.Group className={styles.textBox}>
+            <Form.Label>하단에 에세이를 입력해주세요.</Form.Label>
+            <Form.Control
+              className={styles.textArea}
+              as={"textarea"}
+              rows={13}
+              maxLength={1000}
+              onChange={changeLetterCount}
+            />
+          </Form.Group>
+          <div className={styles.letterCountBox}>
+            <div className={styles.letterCount}>
+              현재 글자수: {letterCount} / 1000
+            </div>
           </div>
-        </div>
-        <div className={styles.submitBtnBox}>
-          <Button variant="primary" type="submit">
-            <img src={submitIcon} alt="로고" />
-            제출하기
-          </Button>
-        </div>
-      </Form>
-    </div>
+          <div className={styles.submitBtnBox}>
+            <Button variant="primary" type="submit">
+              <img src={submitIcon} alt="제출 아이콘" />
+              제출하기
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </>
   );
 }
 
