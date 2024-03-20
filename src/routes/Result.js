5481ac70ba4ffc43f8d5997e49e67fc26f9df9fb
Chori <yidodeveloper@gmail.com>
2024-03-20 21:44:20 +0900

    Design: CSS 적용
    Import CSS in Result.js
    className에 CSS 모듈 적용
    돌아가기 아이콘과 출력 아이콘 추가

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -1,32 +1,35 @@
 import { Form, Button } from "react-bootstrap";
 import Navbars from "../components/Navbars";
+import styles from "../css/Result.module.css";
+import backIcon from "../images/backIcon.png";
+import printIcon from "../images/printIcon.png";
 
 function Result() {
   return (
     <>
       <Navbars />
-      <div className="container">
+      <div className={styles.container}>
         <Form>
-          <Form.Group className="textBox">
+          <Form.Group className={styles.textBox}>
             <Form.Label>에세이 채점 결과</Form.Label>
             <Form.Control
-              className="textArea"
+              className={styles.textArea}
               as="textarea"
               rows={15}
               maxLength={1000}
               readOnly
             />
           </Form.Group>
-          <div className="btnBox">
+          <div className={styles.btnBox}>
             <Button variant="primary" type="submit">
-              <img src="#" alt="돌아가기 아이콘" />
+              <img src={backIcon} alt="돌아가기 아이콘" />
               다시 쓰기
             </Button>
           </div>
-          <div className="btnContainer">
-            <div className="btnBox">
+          <div className={styles.btnContainer}>
+            <div className={styles.btnBox}>
               <Button variant="primary" type="submit">
-                <img src="#" alt="출력 아이콘" />
+                <img src={printIcon} alt="출력 아이콘" />
                 결과 출력하기
               </Button>
             </div>
