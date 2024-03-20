6d6c7267421a1b485532213ee4bc69f83fdd5c69
Chori <yidodeveloper@gmail.com>
2024-03-20 22:02:44 +0900

    Feat: Result.js에 대한 경로 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
+import Result from "./routes/Result";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path={"/"} element={<Home />} />
+        <Route path={"/result"} element={<Result />} />
       </Routes>
     </div>
   );
